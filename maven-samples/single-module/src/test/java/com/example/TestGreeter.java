9af245f6aedda4f3e91629cdfb3fdf3d88d99fb5
Rahul <89964063+easycloudcompute@users.noreply.github.com>
2024-02-02 12:48:29 -0500

    Update TestGreeter.java

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -20,7 +20,12 @@ public class TestGreeter {
     greeter = new Greeter();
   }
 
- 
+  @Test
+  public void greetShouldIncludeTheOneBeingGreeted() {
+    String someone = "World";
+
+    assertThat(greeter.greet(someone), containsString(someone));
+  }
 
   @Test
   public void greetShouldIncludeGreetingPhrase() {
