7f815e41fb16d5eaf09c2ddb7ee3f3def7c97749
Rahul <89964063+easycloudcompute@users.noreply.github.com>
2024-02-03 00:44:57 -0500

    Update 04-push.sh

--- jenkins-plugin-model/ci/04-push.sh
+++ jenkins-plugin-model/ci/04-push.sh
@@ -5,7 +5,7 @@ if [ -z ${DOCKER_HUB_USER+x} ]
 then 
     echo 'Skipping login - credentials not set' 
 else 
-    docker login -u $DOCKER_HUB_USER -p $DOCKER_HUB_PASSWORD
+    sudo docker login -u $DOCKER_HUB_USER -p $DOCKER_HUB_PASSWORD
 fi
 
-docker push rahulunixsa87/jenkins-demo1:$1
+sudo docker push rahulunixsa87/jenkins-demo1:$1
