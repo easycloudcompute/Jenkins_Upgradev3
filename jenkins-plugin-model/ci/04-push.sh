e8f8fc42a72ff1ebb730f4e43d3ed791a190dc91
Rahul <89964063+easycloudcompute@users.noreply.github.com>
2024-02-03 00:46:29 -0500

    Update 04-push.sh

--- jenkins-plugin-model/ci/04-push.sh
+++ jenkins-plugin-model/ci/04-push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker image build -t rahulunixsa87/jenkins-demo1:$1 -f jenkins-plugin-model/src/demo2-publish/dockerfile .
+docker image build -t rahulunixsa87/jenkins-demo:$1 -f jenkins-plugin-model/src/demo2-publish/dockerfile .
 
 if [ -z ${DOCKER_HUB_USER+x} ]
 then 
@@ -8,4 +8,4 @@ else
     sudo docker login -u $DOCKER_HUB_USER -p $DOCKER_HUB_PASSWORD
 fi
 
-sudo docker push rahulunixsa87/jenkins-demo1:$1
+sudo docker push rahulunixsa87/jenkins-demo:$1
